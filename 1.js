9dc65e8b64f16f39c70c29545ed5cab9b7c2eadb
J00r1111 <74411358+J00r1111@users.noreply.github.com>
2021-09-13 13:21:58 +0200

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,3 +1,4 @@
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.all('/secret', function(req, res, next) {
+    console.log('Accessing the secret section ...');
+    next(); // pass control to the next handler
   });
